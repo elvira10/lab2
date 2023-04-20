1d982067afbafac6ec5f5f6f5314d3b679d154d2
Elvira <adilovaelvira123@gmail.com>
2023-04-21 04:54:13 +0600

    including comments

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -4,6 +4,12 @@ public class MyLinkedList<T> implements MyList {
         private Node next;
         private Node previous;
 
+        /**
+         * Node construction
+         * @param element element value of the node
+         * @param next the reference to the next node
+         * @param previous the reference to the previous node
+         */
         public Node(T element, Node next, Node previous) {
             this.element = element;
             this.next = next;
@@ -11,54 +17,76 @@ public class MyLinkedList<T> implements MyList {
         }
     }
 
-    private Node head;
-    private Node tail;
-    private int size;
+    private Node head; //first node of the linked list
+    private Node tail; //last node of the linked list
+    private int size; //number of nodes in the linked list
 
+    /**
+     * constructor
+     * Initializes empty list
+     */
     public MyLinkedList() {
         head = null;
         tail = null;
         size = 0;
     }
 
+    /**
+     * Returns the number of elements in list
+     * @return the number of elements in list
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Checking whether the linked list contains the element
+     * @param o o the element that is going to be checked
+     * @return true if contains, false if not
+     */
     @Override
     public boolean contains(Object o) {
         Node thisN = head;
         while (thisN != null) {
-            if (thisN.element == o) {
+            if (thisN.element == o) { //return true if the node's element is matching the specified element
                 return true;
             }
-            thisN = thisN.next;
+            thisN = thisN.next; //next node
         }
-        return false;
+        return false; //if not return false
     }
 
+    /**
+     * Adding element to the end of the list
+     * @param item the element that is going to be added
+     */
     @Override
     public void add(Object item) {
-        Node newNode = new Node((T) item, null, tail);
-        if (tail == null) {
+        Node newNode = new Node((T) item, null, tail); //new node
+        if (tail == null) { //set new node as the head if list is empty,
             head = newNode;
-        } else {
+        } else { //set new node as the next node otherwise
             tail.next = newNode;
         }
-        tail = newNode;
+        tail = newNode; //updating the tail
         size++;
     }
 
+    /**
+     * Adding element at the specified index in the list
+     * @param item the element that is going to be added
+     * @param index the index at which to add the element
+     */
     @Override
     public void add(Object item, int index) {
-        checkIndex(index);
-        Node newNode = new Node((T) item, null, null);
-        if (index == size) {
-            if (tail == null) {
+        checkIndex(index); //checking if the index is valid
+        Node newNode = new Node((T) item, null, null); //new node
+        if (index == size) { //add new node at the end if the index is the size of the list
+            if (tail == null) { //set the new node as head and tail if the list is empty
                 head = newNode;
                 tail = newNode;
-            } else {
+            } else { // set the new node as next node of current tail and update reference
                 tail.next = newNode;
                 newNode.previous = tail;
                 tail = newNode;
@@ -79,54 +107,67 @@ public class MyLinkedList<T> implements MyList {
         }
     }
 
+    /**
+     * Removing the first occurrence of the specified element from the list
+     * @param item element that is going to be removed
+     * @return true if removed, false if not
+     */
     @Override
     public boolean remove(Object item) {
         Node current = head;
         Node previous = null;
-        boolean check = false;
+        boolean check = false; //indicator that shows if the element was removed or not
         while (current != null) {
-            if (current.element.equals(item)) {
+            if (current.element.equals(item)) { //set flag to be true, if the current node's element matches the item to be removed
                 check = true;
                 break;
             }
             previous = current;
             current = current.next;
         }
-        if (check) {
-            if (previous == null) {
+        if (check) { //if element is found
+            if (previous == null) { //update the head to the next node, if the element that is going to be removed is head
                 head = current.next;
             } else {
-                previous.next = current.next;
+                previous.next = current.next; //update the reference
             }
-            if (current == tail) {
+            if (current == tail) { //ppdate the tail to the previous, if the element that is going to be removed is the tail
                 tail = previous;
             }
             size--;
         }
-        return check;
+        return check; //return boolean
     }
 
+    /**
+     * Removing an element at the specified index in the list
+     * @param index element that is going to be removed
+     * @return the removed element
+     */
     @Override
     public Object remove(int index) {
-        checkIndex(index);
+        checkIndex(index); //checking if the index is valid
         Node current = head;
         Node previous = null;
         for (int i = 0; i < index; i++) {
-            previous = current;
-            current = current.next;
+            previous = current; //move the previous reference to the current node
+            current = current.next; //next node
         }
-        if (previous == null) {
+        if (previous == null) { //remove the head node if previous is null
             head = current.next;
-        } else {
+        } else { //update the next reference of the previous node if not null
             previous.next = current.next;
         }
-        if (current == tail) {
+        if (current == tail) { //update the tail to the previous, if the current is tail
             tail = previous;
         }
-        size--;
-        return current.element;
+        size--; //elements are being removed
+        return current.element; //return removed element
     }
 
+    /**
+     * Clears all elements in list
+     */
     @Override
     public void clear() {
         size = 0;
@@ -134,71 +175,89 @@ public class MyLinkedList<T> implements MyList {
         tail = null;
     }
 
+    /**
+     * Retrieving an element at the specified index in the list
+     * @param index element that is going to be retrieved
+     * @return the element at the specified index
+     */
     @Override
     public Object get(int index) {
-        checkIndex(index);
-        Node thisN;
-        if (index < size / 2) {
-            thisN = head;
-            for (int i = 0; i < index; i++) {
+        checkIndex(index); //checking if the index is valid
+        Node thisN; //node reference
+        if (index < size / 2) { //first half of the list
+            thisN = head; //start from the head
+            for (int i = 0; i < index; i++) { //iteration till we get to the specified index
                 thisN = thisN.next;
             }
-        } else {
-            thisN = tail;
-            for (int i = size - 1; i > index; i--) {
+        } else { //second half of the list
+            thisN = tail; //start from the tail
+            for (int i = size - 1; i > index; i--) { //iteration backwards till we get to the specified index
                 thisN = thisN.previous;
             }
         }
-        return thisN.element;
+        return thisN.element; //return the element at the specified index
     }
 
+    /**
+     * Returning index of the first occurrence of the specified element in the list
+     * @param o the element that is going to be searched
+     * @return index of the first occurrence of searched element, return -1 if not found
+     */
     @Override
     public int indexOf(Object o) {
         int index = 0;
         Node current = head;
-        while (current != null) {
-            if (current.element.equals(o)) {
+        while (current != null) { //iteration in the list
+            if (current.element.equals(o)) { // return the index if current element is equal to the specified
                 return index;
             }
-            current = current.next;
-            index++;
+            current = current.next; //nex node
+            index++; //next index
         }
-        return -1;
+        return -1; //if not found
     }
 
+    /**
+     * Returning index of the last occurrence of the specified element in the list
+     * @param o the element that is going to be searched
+     * @return index of the last occurrence of searched element, return -1 if not found
+     */
     @Override
     public int lastIndexOf(Object o) {
         Node current = head;
-        int index = -1;
+        int index = -1; //initializing index as -1
         int currentIndex = 0;
-        while (current != null) {
-            if (current.element.equals(o)) {
+        while (current != null) { //iteration in the list
+            if (current.element.equals(o)) { // update to the current, if current element is equal to the specified
                 index = currentIndex;
             }
-            currentIndex++;
-            current = current.next;
+            currentIndex++; //next index
+            current = current.next; //next node
         }
         return index;
     }
 
+    /**
+     * Sorts the elements in the linked list in ascending order
+     */
     @Override
     public void sort() {
         Node current = head;
-        while (current != null) {
-            Node min = current;
-            Node inner = current.next;
-            while (inner != null) {
-                if (((Comparable<T>) inner.element).compareTo(min.element) < 0) {
+        while (current != null) { //iteration in the list
+            Node min = current; //initializing current node as min
+            Node inner = current.next; //compartment with next
+            while (inner != null) { //iteration of remaining
+                if (((Comparable<T>) inner.element).compareTo(min.element) < 0) { // update the min node to be the inner node if inner node element is smaller than min node
                     min = inner;
                 }
-                inner = inner.next;
+                inner = inner.next; //move to the next node
             }
-            if (min != current) {
+            if (min != current) { // If the min node is not the current node, swap the elements
                 T temp = current.element;
                 current.element = min.element;
                 min.element = temp;
             }
-            current = current.next;
+            current = current.next; // move to the next node in the list
         }
     }
 }
